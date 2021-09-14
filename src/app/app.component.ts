268f40848998970ccf3744c4bb5115cb9aa9b5db
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-09-14 13:22:01 +0200

    Add additional component and minimal style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
+  // Otherwise, inline-styling
+  /* styles: [`
+  h1 {
+      color: red;
+      text-align: center;
+  }`] */
 })
 export class AppComponent {
   title = 'angular-app';
