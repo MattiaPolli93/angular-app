9e50db3704f0ea760297a91765e400a7849a482a
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-09-14 14:52:49 +0200

    Implement basic dynamic data functionalities

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-    template: '<app-server></app-server><app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+    selector: 'app-servers',
+    /* template: '<app-server></app-server><app-server></app-server>', */
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = "No server was created!";
 
-  constructor() { }
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = "Server was created!";
+    }
 
 }
