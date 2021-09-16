43ca22bba92efb74ea35140b10f1184912b40a44
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-09-16 12:41:50 +0200

    Implement a recipe-test

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-    recipes: [];
+    recipes: Recipe[] = [
+        new Recipe("A Test Recipe", "This is a descriptive test", "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"),
+        new Recipe("A Test Recipe", "This is a descriptive test", "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg")
+    ];
 
     constructor() { }
 
